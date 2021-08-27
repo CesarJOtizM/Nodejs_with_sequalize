ee22ea5cb028aa56299ba66512cc50a61cd946c8
CesarJOtizM <cesarjavierortizmontero@gmail.com>
2021-08-26 21:25:04 -0500

    heroku deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,5 +4,11 @@ import { config } from './ConfigDB';
 const sequelize = new Sequelize(config.DB, config.userDB, config.password, {
   host: config.host,
   dialect: 'mysql',
+  pool: {
+    max: 100,
+    min: 0,
+    idle: 200000,
+    acquire: 1000000,
+  },
 });
 export default sequelize;
